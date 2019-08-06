0dbe51c271a3e5c90b246953c0f68e631e5bf10f
serhii <serhii@MBP-Serhii.Dlink>
2019-08-07 00:58:44 +0300

    added hello controller

--- src/main/java/com/pharmacy/pharmacybackend/PharmacyBackendApplication.java
+++ src/main/java/com/pharmacy/pharmacybackend/PharmacyBackendApplication.java
@@ -2,12 +2,24 @@ package com.pharmacy.pharmacybackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.gcp.autoconfigure.storage.GcpStorageAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, GcpStorageAutoConfiguration.class})
 public class PharmacyBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PharmacyBackendApplication.class, args);
 	}
+}
 
+@RestController
+class HelloController {
+	@GetMapping("/hello/{name}")
+	public String hello(@PathVariable String name) {
+		return "Hello, " + name + "!";
+	}
 }
